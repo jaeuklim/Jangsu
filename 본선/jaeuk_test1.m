be0d4a54b48522c830c1434c447884a62396428e
jeongdonggi <dkrhank45@gmail.com>
2022-07-06 18:42:18 +0900

    코드

--- 본선/jaeuk_test1.m
+++ 본선/jaeuk_test1.m
@@ -2,7 +2,7 @@
 thdown_blue = [0.55, 0.43, 0.25];
 thup_blue = [0.75, 1, 1];
 
-drone = ryze()
+drone = ryze();
 cameraObj = camera(drone);
 takeoff(drone);
 moveup(drone, 'distance', 0.6);
@@ -15,9 +15,6 @@ while(1)
     [rows, cols, channels] = size(frame_hsv);       
     dst_hsv1 = double(zeros(size(frame_hsv)));      
     dst_hsv2 = double(zeros(size(frame_hsv)));
-    dst_h = dst_hsv1(:, :, 1);
-    dst_s = dst_hsv1(:, :, 2);
-    dst_v = dst_hsv1(:, :, 3);
 
     for row = 1:rows
         for col = 1:cols
@@ -33,14 +30,10 @@ while(1)
 
     dst_rgb1 = hsv2rgb(dst_hsv1);
     dst_rgb2 = hsv2rgb(dst_hsv2);
+    figure,imshow(dst_rgb1)
     dst_gray = rgb2gray(dst_rgb1);
 
     corners1 = pgonCorners(dst_gray, 4);       % 바깥사각형 코너 좌표 검출
-
-    p1 = corners1(4, :);         % 좌상단
-    p2 = corners1(3, :);         % 우상단
-    p3 = corners1(1, :);         % 좌하단
-    p4 = corners1(2, :);         % 우하단
 
     roi_x = [corners1(1, 2) + 5, corners1(2, 2) - 5, corners1(3, 2) - 5, corners1(4, 2) + 5];  % roi범위 소량 확장
     roi_y = [corners1(1, 1) - 5, corners1(2, 1) - 5, corners1(3, 1) + 5, corners1(4, 1) + 5];  % roi범위 소량 확장
@@ -58,15 +51,15 @@ while(1)
                 count_pixel = count_pixel + 1;      %검출될때마다 픽셀수 세기
                 center_row = center_row + row;      %검출될때마다 가로좌표 더하기
                 center_col = center_col + col;      %검출될때마다 세로좌표 더하기
-            end        
+            end
         end
     end
 
     center_row = center_row / count_pixel;
     center_col = center_col / count_pixel;
     
-    answer = [center_col, center_row]          % 센터좌표 검출
-
+    answer = [center_col, center_row];          % 센터좌표 검출
+    %{
     subplot(2, 3, 1); imshow(frame);
     subplot(2, 3, 2); imshow(dst_rgb1);
     subplot(2, 3, 3); imshow(dst_rgb2);
@@ -74,7 +67,8 @@ while(1)
     subplot(2, 3, 5); imshow(dst_gray); hold on;
     plot(center_col, center_row, 'r*'); hold off;
     subplot(2, 3, 6); imshow(frame); hold on;
-    plot(center_col, center_row, 'r*'); hold off;   
+    plot(center_col, center_row, 'r*'); hold off;
+    %}
 end
 
 %land(droneObj);
